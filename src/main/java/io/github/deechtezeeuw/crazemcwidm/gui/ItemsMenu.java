cc7a05d047717b71aa1d57f404e651cef0d785c2
DeEchteZeeuw <rubendalebout@gmail.com>
2022-04-04 19:37:46 +0200

    Fixed Items menu on the GameDataManager

--- src/main/java/io/github/deechtezeeuw/crazemcwidm/gui/ItemsMenu.java
+++ src/main/java/io/github/deechtezeeuw/crazemcwidm/gui/ItemsMenu.java
@@ -19,12 +19,19 @@ public class ItemsMenu extends GraphicalUserInterface {
 
     @Override
     public void open(Player player) {
-        Game game = plugin.getSQL().sqlSelect.playerGame(player.getUniqueId());
+        Game game = (plugin.getGameDataManager().alreadyHosting(player.getUniqueId())) ? plugin.getGameDataManager().getHostingGame(player.getUniqueId()) : null;
 
         if (game == null) {
             player.closeInventory();
             player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                    plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cEr is iets fout gegaan!"));
+                    plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cEr is geen game vonden die jij host, het menu sluit!"));
+            return;
+        }
+
+        if (!game.getMap().equals(player.getWorld().getUID())) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cJe bent niet in de correcte wereld, het menu sluit!"));
+            player.closeInventory();
             return;
         }
 
