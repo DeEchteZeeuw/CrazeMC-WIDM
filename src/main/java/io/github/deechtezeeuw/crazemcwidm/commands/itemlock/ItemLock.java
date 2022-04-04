6b9d37a5d098264975a31f793e5b3b03b1969002
DeEchteZeeuw <rubendalebout@gmail.com>
2022-04-04 20:41:16 +0200

    Item lock and undroppable command fixed

--- src/main/java/io/github/deechtezeeuw/crazemcwidm/commands/itemlock/ItemLock.java
+++ src/main/java/io/github/deechtezeeuw/crazemcwidm/commands/itemlock/ItemLock.java
@@ -3,6 +3,7 @@ package io.github.deechtezeeuw.crazemcwidm.commands.itemlock;
 import io.github.deechtezeeuw.crazemcwidm.CrazeMCWIDM;
 import io.github.deechtezeeuw.crazemcwidm.classes.Game;
 import io.github.deechtezeeuw.crazemcwidm.commands.Commands;
+import io.github.deechtezeeuw.crazemcwidm.gui.PanelMenu;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -31,23 +32,35 @@ public class ItemLock extends Commands {
 
         Player player = (Player) sender;
 
-        if (!plugin.getSQL().sqlSelect.playerIsHost(player.getUniqueId())) {
+        // Check if host
+        if (!plugin.getGameDataManager().alreadyHosting(player.getUniqueId())) {
             player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                     plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cJe host geen game!"));
             return;
         }
 
-        Game game = plugin.getSQL().sqlSelect.playerHostGame(player.getUniqueId());
+        Game game = (plugin.getGameDataManager().alreadyHosting(player.getUniqueId())) ? plugin.getGameDataManager().getHostingGame(player.getUniqueId()) :  null;
 
-        if (!player.getWorld().getUID().equals(game.getMap())) {
+        if (game == null) {
+            player.closeInventory();
             player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                    plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cJe bent niet in de juiste game om dit te doen."));
+                    plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cEr is geen game vonden die jij host, het menu sluit!"));
+            new PanelMenu().open(player);
+            return;
+        }
+
+        if (!game.getMap().equals(player.getWorld().getUID())) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cJe bent niet in de correcte wereld, het menu sluit!"));
+            player.closeInventory();
+            new PanelMenu().open(player);
             return;
         }
 
         // Check if player has item in hand
         if (player.getInventory().getItemInMainHand() == null) {
-            player.sendMessage("Je moet een item in je hand hebben hiervoor!");
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    plugin.getConfigManager().getMain().serverPrefix + plugin.getConfigManager().getMain().serverDivider + "&cJe moet hiervoor een item in je handen hebben!"));
             return;
         }
 
@@ -62,7 +75,7 @@ public class ItemLock extends Commands {
 
         try {
             ItemMeta itemMeta = item.getItemMeta();
-            List<String> itemLore = (itemMeta.getLore() == null) ? new ArrayList<String>() : itemMeta.getLore();
+            List<String> itemLore = (itemMeta.getLore() == null) ? new ArrayList<>() : itemMeta.getLore();
             String text = ChatColor.translateAlternateColorCodes('&', "&8>> &c&lLocked");
             itemLore.add(text);
             itemMeta.setLore(itemLore);
